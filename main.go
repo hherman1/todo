195c40abec0247125762f33b60a51729d503e4be
Hunter Herman <hherman@roblox.com>
2024-06-30 21:44:37 -0700

    fix

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ Todo is a command-line and acme client for a to-do task tracking system.
 	usage: todo [-a] [-e] [-d subdir] [-done] <query>
 
 Todo runs the query and prints the maching tasks, one per line.
-If the query is a single task number, as in ``todo 1'', todo prints
+If the query is a single task number, as in “todo 1”, todo prints
 the full history of the task.
 
 The -a flag opens the task or query in an acme window.
@@ -16,7 +16,6 @@ The -e flag opens the task or query in the system editor.
 
 The exact acme/editor integration remains undocumented
 but is similar to acme mail or to rsc.io/github/issue.
-
 */
 package main
 
@@ -35,7 +34,7 @@ import (
 )
 
 var (
-	acmeFlag = flag.Bool("a", false, "open in new acme window")
+	acmeFlag = flag.Bool("a", true, "open in new acme window")
 	editFlag = flag.Bool("e", false, "edit in system editor")
 	dirFlag  = flag.String("d", "", "todo subdirectory")
 	doneFlag = flag.Bool("done", false, "mark matching todos as done")
